972e99260c0886a8681968a29881a103327e6121
nwunderly <nwunder.dev@gmail.com>
2026-01-06 21:05:56 -0500

    run black & isort

--- gendotenv.py
+++ gendotenv.py
@@ -3,7 +3,4 @@ import secrets
 token = secrets.token_hex(64)
 
 with open(".env", "w") as fp:
-    fp.write(
-        f'FLASK_SECRET={token}\n'
-        f'VIDEO_SOURCE=1'
-    )
+    fp.write(f"FLASK_SECRET={token}\n" f"VIDEO_SOURCE=1")
--- test_webcam.py
+++ test_webcam.py
@@ -3,27 +3,27 @@ import subprocess
 import time
 
 import dotenv
+
 dotenv.load_dotenv()
 
 # must be done after loading dotenv
 import cv2
-
 
 VIDEO_SOURCE = int(os.getenv("VIDEO_SOURCE"))
 
 
 def get_screen_resolution():
     # Get screen resolution on Linux
-    output = subprocess.check_output(['xrandr']).decode()
+    output = subprocess.check_output(["xrandr"]).decode()
 
     screen_width = None
     screen_height = None
 
     # Parse for current resolution (bit hacky but works)
-    for line in output.split('\n'):
-        if '*' in line:
+    for line in output.split("\n"):
+        if "*" in line:
             resolution = line.split()[0]
-            screen_width, screen_height = map(int, resolution.split('x'))
+            screen_width, screen_height = map(int, resolution.split("x"))
             break
 
     if not (screen_width and screen_height):
@@ -59,11 +59,11 @@ def test_webcam():
                 print("Error: Failed to capture image.")
                 break
 
-            window_name = 'Webcam'
+            window_name = "Webcam"
             cv2.namedWindow(window_name)
 
             # Display the resulting frame
-            cv2.imshow('Webcam', frame)
+            cv2.imshow("Webcam", frame)
 
             # Get monitor/window dimensions
             screen_width, screen_height = get_screen_resolution()
@@ -75,7 +75,7 @@ def test_webcam():
             cv2.moveWindow(window_name, x, y)
 
             # Press 'q' on the keyboard to exit the loop
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
     finally:
